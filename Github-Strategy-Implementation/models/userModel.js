54fb1d1d3988dee445da1945e58694cac0058bfb
Yeshwanth <vryeshwanth06@gmail.com>
2025-10-15 19:27:56 +0530

    Implement db queries

---
+++ Github-Strategy-Implementation/models/userModel.js
@@ -0,0 +1,11 @@
+import pool from "../db.js";
+
+export async function findUserByGithubId(githubId) {
+    const result = await pool.query("SELECT * FROM authUsers WHERE github_id = $1", [githubId]);
+    return result.rows[0];
+}
+
+export async function createUser(githubId, username) {
+    const result = await pool.query("INSERT INTO authUsers (github_id, username) VALUES ($1, $2) RETURNING *", [githubId, username]);
+    return result.rows[0];
+}
