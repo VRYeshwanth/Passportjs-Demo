4805849dc909b71e2aa043cc1d112f1e1f23be38
Yeshwanth <vryeshwanth06@gmail.com>
2025-10-16 17:51:12 +0530

    Setup passport config

---
+++ Github-Strategy-Implementation/config/passportConfig.js
@@ -0,0 +1,56 @@
+import { Strategy } from "passport-github2";
+import dotenv from "dotenv";
+import { createUser , findUserByGithubId } from "../models/userModel.js";
+
+dotenv.config()
+
+export function initialize(passport) {
+
+    // Register the Github Strategy
+
+    // This tells Passport to use the above authentication function
+    // when we call passport.authenticate('github') in our routes.
+    passport.use(new Strategy(
+        {
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: process.env.GITHUB_CALLBACK_URL
+        },
+        async (accessToken, refreshToken, profile, done) => {
+            try {
+                let user = await findUserByGithubId(profile.id);
+
+                if(!user) {
+                    user = await createUser({
+                        githubId: profile.id,
+                        username: profile.username
+                    });
+                }
+
+                return done(null, user);
+            }
+            catch(err) {
+                return done(err);
+            }
+        }
+    ))
+
+    // Serialize User
+    
+    // After successful login, Passport saves user info to the session.
+    // We only store the user.id to keep the session lightweight.
+    passport.serializeUser((user, done) => done(null, user.id));
+
+    // Deserialize User
+    
+    // For every request after login, Passport retrieves user.id from the session,then fetches the full user details from the database.
+    // The result is attached to req.user in all routes.
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await findUserByGithubId(id);
+            done(null, user);
+        } catch (err) {
+            done(err);
+        }
+    });
+}
